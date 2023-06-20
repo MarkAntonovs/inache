f2ad3c52f350389525b3400108d5da60d842f70e
Марк Антонов <dbasbkfga@Marks-MacBook-Air.local>
2023-06-20 11:04:08 +0300

    added page

---
+++ assets/object.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>'
+    });
+  });
+  
