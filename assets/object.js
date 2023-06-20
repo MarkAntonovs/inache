c20a1c61ce85c15a80d44cdb4fd5445f4f6b3536
Марк Антонов <dbasbkfga@Marks-MacBook-Air.local>
2023-06-20 21:38:26 +0300

    hope finished

--- assets/object.js
+++ assets/object.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-    $('.slider').slick({
-      arrows: true,
-      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-      nextArrow: '<button type="button" class="slick-next">Next</button>'
-    });
+  $('.slider').slick({
+      arrows:true, // показать стрелки
+      dots:false, // не показывать точки
+      autoplay:true, // автоматическое проигрывание слайдов
   });
-  
+});
